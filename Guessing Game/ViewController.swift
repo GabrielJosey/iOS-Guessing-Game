1f60157ca575e6a6675d94cac73c95e29aa8bbb0
Gabriel Josey <Acts2024Life@gmail.com>
2018-03-09 14:24:48 -0500

    added userfeed back as well as changed the background color to a treehouse like Green.

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -12,15 +12,24 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var userGuessField: UITextField!
     
+    @IBOutlet weak var rulesLabel: UILabel!
+    
+    @IBOutlet weak var userFeedback: UILabel!
+    
     
     var randomNumber = Int(arc4random_uniform(100))
     var userGuess: Int!
     
+    var rulesText = """
+    Guess a number between 1 and 100.
+    You have 5 tries to guess the correct number.
+    You will be told if the number is higher or lower.
+"""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        rulesLabel.text = rulesText
         
     }
 
