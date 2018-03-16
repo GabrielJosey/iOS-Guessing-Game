8d06b2591acb4fc8b14b831414e2007ffb89984d
Gabriel J <Acts2024Life@gmail.com>
2018-03-16 12:59:33 -0400

    Added features for when user fails. Prevented user from being able to edit after failure

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -20,16 +20,20 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var theCount: UIImageView!
     
+    @IBOutlet weak var failMonkey: UIImageView!
+    
+    
     var randomNumber = Int(arc4random_uniform(10))
     var userGuess: Int!
     
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         thumbsUp.isHidden = true
         theCount.isHidden = false
+        failMonkey.isHidden = true
     }
     
     override func didReceiveMemoryWarning() {
@@ -44,25 +48,32 @@ class ViewController: UIViewController {
         
         userGuess = Int(userGuessField.text!)
         
-        
-            if userGuessField.text == "" {
-                userFeedback.text = "Please enter a number"
-            } else if Int(userGuessField.text!)! > randomNumber {
-                userFeedback.text = "Too High. Guess again."
-                guesses += 1
-                //userGuessField.text = ""
-            } else if Int(userGuessField.text!)! < randomNumber {
-                userFeedback.text = "Too low. Bump your guess up!"
-                guesses += 1
-               // userGuessField.text = ""
-            } else if Int(userGuessField.text!)! == randomNumber {
-                userFeedback.text = "WAY TO GO! You guessed the number! Tap the monkey to play again."
-                view.endEditing(true)
-                theCount.isHidden = true
-                thumbsUp.isHidden = false
-                guesses = 5
-               
-            }
+        if guesses == 4 {
+            view.endEditing(true)
+            theCount.isHidden = true
+            thumbsUp.isHidden = true
+            failMonkey.isHidden = false
+            guesses = 5
+            userGuessField.isEnabled = false
+            userFeedback.text = "Oh! So close! You ran out of tries. Tap the monkey to try again."
+        } else if userGuessField.text == "" {
+            userFeedback.text = "Please enter a number"
+        } else if Int(userGuessField.text!)! > randomNumber {
+            userFeedback.text = "Too High. Guess again."
+            guesses += 1
+            //userGuessField.text = ""
+        } else if Int(userGuessField.text!)! < randomNumber {
+            userFeedback.text = "Too low. Bump your guess up!"
+            guesses += 1
+            // userGuessField.text = ""
+        } else if Int(userGuessField.text!)! == randomNumber {
+            userFeedback.text = "WAY TO GO! You guessed the number! Tap the monkey to play again."
+            view.endEditing(true)
+            theCount.isHidden = true
+            thumbsUp.isHidden = false
+            guesses = 5
+            
+        }
         
         userGuessField.text = ""
     }
@@ -71,9 +82,13 @@ class ViewController: UIViewController {
     
     @IBAction func resetGame(_ sender: Any) {
         thumbsUp.isHidden = true
+        failMonkey.isHidden = true
         theCount.isHidden = false
+        theCount.transform.rotated(by: 0.55)
         userFeedback.text = ""
         randomNumber = Int(arc4random_uniform(10))
+        guesses = 0
+        userGuessField.isEnabled = true
         
     }
     
